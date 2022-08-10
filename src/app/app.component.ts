4ac5539ab74e1b4984e41f8c0f69dc5bdfc7d18a
gnaquin <graham@naquin.live>
2022-08-10 16:32:55 -0400

    working on bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: "Return Home", url: '', icon: 'home' },
+    { title: "Home", url: '', icon: 'home' },
     { title: "I'm a Contractor", url: 'contractor-form', icon: 'build' },
     { title: 'Contact Us', url: 'contact-form', icon: 'mail' },
     { title: 'Submit a Ticket', url: 'customer-form', icon: 'pricetag' },
