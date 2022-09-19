0a7ce231fbcf4a3c58538f1deb2cd30843f6a711
Graham Naquin <graham@naquin.live>
2022-09-19 01:21:29 -0500

    working on new header in desktop view

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { HomePageRoutingModule } from '../home/home-routing.module';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "../components/footer/footer.component";
+import { DesktopHeaderComponent } from "./desktop-header/desktop-header.component";
 
 @NgModule ({
     imports: [
         HomePageRoutingModule
     ],
-    declarations: [HeaderComponent, FooterComponent],
-    exports: [HeaderComponent, FooterComponent]
+    declarations: [HeaderComponent, FooterComponent, DesktopHeaderComponent],
+    exports: [HeaderComponent, FooterComponent, DesktopHeaderComponent]
 })
 
 export class ComponentsModule{}
